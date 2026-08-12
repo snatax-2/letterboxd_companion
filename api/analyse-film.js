25b11576b25c579454ea5bd26ea38f3d2d13b665
Snatax-12 <chiarellidario2@gmail.com>
2026-08-12 20:51:28 +0200

    description du changement

--- api/analyse-film.js
+++ api/analyse-film.js
@@ -57,7 +57,7 @@ export default async function handler(req, res) {
 
   try {
     const r = await fetch(
-      'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent',
+      'https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent',
       {
         method: 'POST',
         headers: {
